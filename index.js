6a02226ef09e84da9d79c9ebd020bfce37427033
taherul77 <taherul2317@gmail.com>
2023-06-06 13:08:17 +0600

    update project

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const express= require('express');
 const app =express();
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
 require('dotenv').config();
+const stripe = require('stripe')(process.env.PAYMENT_KEY);
+
 const port = process.env.PORT || 5000;
 
 //middleware
@@ -9,11 +12,27 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+const verifyJWT = (req,res,next) =>{
+const authorization = req.headers.authorization;
+
+if(!authorization){
+  return res.status(401).send({error: true , message: 'unauthorized access'});
+  
+} 
+const token = authorization.split(' ')[1];
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET,(err,decoded)=>{
+    if(err){
+      return res.status(401).send({error: true, message: 'unauthorized access'})
+    }
+    req.decoded =decoded;
+    next();
+  })
+
+}
 
 
 
-
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.iroozgm.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -34,13 +53,100 @@ async function run() {
 const menuCollection = client.db("bistro").collection("menu");
 const reviewCollection = client.db("bistro").collection("review");
 const cartCollection = client.db("bistro").collection("cart");
+const userCollection = client.db("bistro").collection("user");
 
-app.get('/menu',async(req,res)=>{
+app.post('/jwt',(req,res)=>{
+  user = req.body;
+  const token = jwt.sign(user,process.env.ACCESS_TOKEN_SECRET,{expiresIn: '5h'});
+  res.send({token});
+})
+const verifyAdmin = async(req,res,next)=>{
+  const email =req.decoded.email;
+  const query = {email:email}
+  const user = await userCollection.findOne(query);
+  if(user?.role !== 'admin' ){
+    return res.status(403).send({ error: true , message: 'forbidden message'})
+  }
+  next();
+}
+
+
+app.get('/users',verifyJWT,verifyAdmin,  async(req,res)=>{
+const result = await userCollection.find().toArray();
+res.send(result);
+
+})
+
+
+
+app.post('/users',async(req,res)=>{
+  const user = req.body;
+  const email = user.email;
+  const query = {email};
+  const existingUser = await userCollection.findOne(query);
+  if (existingUser) {
+   return res.send( {message: ''});
+  }
+  
+ 
+  const result = await userCollection.insertOne(user);
+  res.send(result);
+
+
+})
+
+app.get('/users/admin/:email',verifyJWT,async(req,res)=>{
+
+  const email= req.params.email;
+  if(req.decoded.email !== email){
+    res.send({admin:false})
+  }
+  const query = {email:email}
+  const user = await userCollection.findOne(query);
+  const result = { admin : user?.role === 'admin'}
+  res.send(result);
+})
+
+app.patch('/users/admin/:id',async(req,res)=>{
+  const id= req.params.id;
+  const filter = {_id: new ObjectId(id)}
+  const updateDoc ={
+    $set: {
+      role: 'admin'
+    },
+    
+  };
+  const result = await userCollection.updateOne(filter,updateDoc);
+    res.send(result);
+})
+
+
+
+
+
+
+
+app.get('/menu', async(req,res)=>{
   const cursor = menuCollection.find();
   const result = await cursor.toArray();
   res.send(result);
 
 })
+
+
+app.post('/menu',  async (req, res) => {
+  const newItem = req.body;
+  const result = await menuCollection.insertOne(newItem)
+  res.send(result);
+})
+app.delete('/menu/:id', verifyJWT, verifyAdmin, async (req, res) => {
+  const id = req.params.id;
+  const query = { _id: new ObjectId(id) }
+  const result = await menuCollection.deleteOne(query);
+  res.send(result);
+})
+
+
 app.get('/review',async(req,res)=>{
   const cursor = reviewCollection.find();
   const result = await cursor.toArray();
@@ -48,11 +154,15 @@ app.get('/review',async(req,res)=>{
 
 })
 
-app.get('/cart', async(req, res)=>{
+app.get('/cart',verifyJWT, async(req, res)=>{
 
 const email = req.query.email;
 if(!email){
   res.send([]);
+}
+const decodedEmail = req.decoded.email;
+if(email !== decodedEmail){
+  return req.status(403).send({error: true, message: 'forbidden access'})
 }
 const query = { email : email};
 const result = await cartCollection.find(query).toArray();
@@ -70,6 +180,36 @@ app.post('/cart', async(req,res)=>{
   res.send(result);
 })
 
+app.delete('/cart/:id', async (req,res)=>{
+const id =req.params.id;
+console.log(id);
+const query = {_id: new ObjectId(id)};
+const result = await cartCollection.deleteOne(query);
+res.send(result)
+
+})
+
+app.post('/create-payment-intent',verifyJWT, async(req,res)=>{
+  const {price} = req.body;
+  const amount= price*100;
+  const paymentIntent = await stripe.paymentIntents.create({
+    amount:amount,
+    currency: 'usd',
+    payment_method_types: ['card']
+  });
+  res.send({
+    clientSecret: paymentIntent.client_secret
+  })
+
+})
+
+app.post('/payment', verifyJWT,verifyAdmin,async(req,res)=>{
+  const payment = req.body;
+  const insertResult = await paymentCollection.insertOne(payment);
+  const query = { _id: { $in: payment.cartItems.map(id => new ObjectId(id))}}
+  const deleteResult = await cartCollection.deleteMany(query)
+  res.send({insertResult,deleteResult});
+})
 
 
 
